26eeedb33b1152c5e16f15c0478999d40700d2b5
Maksym Korchynskyi <maksym.24.09.2003@gmail.com>
2024-12-21 17:38:26 +0200

    update solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,9 @@
 'use strict';
 
 function createCopy(table, columnIndex, copyTo) {
-  const column = getColumn(table, columnIndex);
-
-  changeColumn(table, column, copyTo);
-}
-
-function changeColumn(tablet, column, copyTo) {
-  column.forEach((element, i) => {
-    if (copyTo === undefined) {
-      tablet.rows[i].append(element.cloneNode(true));
-    } else {
-      tablet.rows[i].cells[copyTo].innerHTML = element.innerHTML;
-    }
+  [...table.rows].forEach((row) => {
+    row.insertBefore(row.cells[columnIndex].cloneNode(true), row.cells[copyTo]);
   });
 }
 
-function getColumn(tablet, column) {
-  return [...tablet.rows].map((row) => row.cells[column]);
-}
-
-createCopy(document.querySelector('table'), 4);
 createCopy(document.querySelector('table'), 1, 4);
