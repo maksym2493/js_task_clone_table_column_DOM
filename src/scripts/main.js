334e41e863b68d25816c8d03a4e1653b9ce29734
Maksym Korchynskyi <maksym.24.09.2003@gmail.com>
2024-12-21 17:25:44 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+function createCopy(table, columnIndex, copyTo) {
+  const column = getColumn(table, columnIndex);
+
+  changeColumn(table, column, copyTo);
+}
+
+function changeColumn(tablet, column, copyTo) {
+  column.forEach((element, i) => {
+    if (copyTo === undefined) {
+      tablet.rows[i].append(element.cloneNode(true));
+    } else {
+      tablet.rows[i].cells[copyTo].innerHTML = element.innerHTML;
+    }
+  });
+}
+
+function getColumn(tablet, column) {
+  return [...tablet.rows].map((row) => row.cells[column]);
+}
+
+createCopy(document.querySelector('table'), 4);
+createCopy(document.querySelector('table'), 1, 4);
